ec19ba8de36ccdbc2211da616f164daf82955840
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2020-06-18 14:54:36 +0200

    order of brs definition changed

--- de.tudresden.inf.st.bigraphs.dsl.web/WebRoot/xtext-resources/generated/mode-bdsl.js
+++ de.tudresden.inf.st.bigraphs.dsl.web/WebRoot/xtext-resources/generated/mode-bdsl.js
@@ -1,6 +1,6 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
-		var keywords = "UNSET|UNSET2|active|agents|arity|atomic|brs|ctrl|ecore|id|import|iso|main|partial|passive|pred|preds|println|react|rules|signature|val|xmi";
+		var keywords = "UNSET|UNSET2|active|agents|arity|atomic|brs|ctrl|ecore|id|import|iso|main|merge|partial|passive|pred|preds|println|react|rules|signature|val|xmi";
 		this.$rules = {
 			"start": [
 				{token: "comment", regex: "\\/\\/.*$"},
