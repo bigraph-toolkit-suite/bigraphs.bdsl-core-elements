942f6423ccf2af10bf123d848cb0c38a8fd59c79
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2020-06-24 18:53:19 +0200

    export method rule added

--- de.tudresden.inf.st.bigraphs.dsl.web/WebRoot/xtext-resources/generated/mode-bdsl.js
+++ de.tudresden.inf.st.bigraphs.dsl.web/WebRoot/xtext-resources/generated/mode-bdsl.js
@@ -1,6 +1,6 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
-		var keywords = "UNSET|UNSET2|active|agents|arity|atomic|barren|brs|closure|clsre|ctrl|ecore|id|import|iso|main|merge|partial|passive|pred|preds|println|react|rules|signature|subst|substitution|val|xmi";
+		var keywords = "UNSET|UNSET2|active|agents|arity|as|atomic|barren|bigmc|bigrapher|brs|closure|clsre|console|ctrl|ecore|export|file|filename|id|import|iso|main|merge|partial|passive|pred|preds|println|react|rules|signature|subst|substitution|to|val|xmi";
 		this.$rules = {
 			"start": [
 				{token: "comment", regex: "\\/\\/.*$"},
