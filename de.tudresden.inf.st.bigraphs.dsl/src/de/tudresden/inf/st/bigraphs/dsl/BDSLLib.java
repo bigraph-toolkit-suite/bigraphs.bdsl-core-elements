4d3a93f155d27220b8e85b0d4d4c08c11b644337
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2020-08-31 18:49:54 +0200

    same namespace resolver and imports + some tests

--- de.tudresden.inf.st.bigraphs.dsl/src/de/tudresden/inf/st/bigraphs/dsl/BDSLLib.java
+++ de.tudresden.inf.st.bigraphs.dsl/src/de/tudresden/inf/st/bigraphs/dsl/BDSLLib.java
@@ -1,10 +1,15 @@
 package de.tudresden.inf.st.bigraphs.dsl;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
+import org.apache.log4j.lf5.util.ResourceUtils;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -20,15 +25,19 @@ public class BDSLLib {
 
 	}
 
-	public void loadIncludeLibraries(ResourceSet resourceSet, String[] includeFilenames) throws IOException {
+	public List<Resource> loadIncludeLibraries(ResourceSet resourceSet, String[] includeFilenames) throws IOException {
 		if (Objects.isNull(includeFilenames) || includeFilenames.length == 0)
-			return;
+			return Collections.EMPTY_LIST;
+		List<Resource> resources = new ArrayList<>();
 		for (String eachFilename : includeFilenames) {
-			URL url = getClass().getClassLoader().getResource(eachFilename);
+//			URL url = BDSLLib.class.getResource(eachFilename);
+			URL url = new File(eachFilename).toURI().toURL();
 			InputStream stream = url.openStream();
 			Resource resource = resourceSet.createResource(URI.createFileURI(url.getPath()));
 			resource.load(stream, resourceSet.getLoadOptions());
+			resources.add(resource);
 		}
+		return resources;
 	}
 
 }
