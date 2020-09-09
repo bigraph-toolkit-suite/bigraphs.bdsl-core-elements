8a1d6afe36b4535ed7e005eded2bb5f3828f8a16
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2020-09-09 18:07:56 +0200

    loading jar archives for BDSLLib class + singleton

--- de.tudresden.inf.st.bigraphs.dsl/src/de/tudresden/inf/st/bigraphs/dsl/BDSLLib.java
+++ de.tudresden.inf.st.bigraphs.dsl/src/de/tudresden/inf/st/bigraphs/dsl/BDSLLib.java
@@ -1,21 +1,65 @@
 package de.tudresden.inf.st.bigraphs.dsl;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
-import org.apache.log4j.lf5.util.ResourceUtils;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
+/**
+ * An utility class that provides essential functionality for BDSL programs.
+ * <p>
+ * It allows loading multiple BDSL resources into one resource set (e.g., to
+ * exploit the "library approach", or for including multiple BDSL programs).
+ * <p>
+ * Further, this helper class that may add external jar files to the class
+ * loader to make them available for calling user-defined functions.
+ * 
+ * @author Dominik Grzelak
+ */
 public class BDSLLib {
 	public static String MAIN_LIB = "";
+	private final static String FILE_EXT_CLASS = ".class";
+	
+	private static volatile BDSLLib instance;
+	private static Object lock = new Object();
+	private final List<File> loadedJarFiles = new LinkedList<>();
+	
+	private BDSLLib() {
+		
+	}
+	
+	public static BDSLLib getInstance() {
+		BDSLLib result = instance;
+		if (result == null) {
+			synchronized (lock) {
+				result = instance;
+				if (result == null)
+					instance = result = new BDSLLib();
+			}
+		}
+		return result;
+	}
 
 	public void loadStandardLibs(ResourceSet resourceSet) throws IOException {
 		URL url = getClass().getClassLoader().getResource(MAIN_LIB);
@@ -23,6 +67,69 @@ public class BDSLLib {
 		Resource resource = resourceSet.createResource(URI.createFileURI(url.getPath()));
 		resource.load(stream, resourceSet.getLoadOptions());
 
+	}
+
+	public List<Class> findAllClasses(String packageName, Class<?> classFilter, List<JarFile> jarFiles) {
+		Set<Class<?>> clazzesFound = new HashSet<Class<?>>();
+		String packageName2 = packageName.replaceAll("\\.", "/");
+		for (JarFile file : jarFiles) {
+			URL[] urls;
+			try {
+				urls = new URL[] { new URL("jar:file:" + file.getName() + "!/") };
+				URLClassLoader cl = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
+				Enumeration<JarEntry> e = file.entries();
+				while (e.hasMoreElements()) {
+					JarEntry entry = e.nextElement();
+					if (entry.getName().startsWith(packageName2) && entry.getName().endsWith(FILE_EXT_CLASS)) {
+						String className = entry.getName();
+						className = className.substring(0, className.length() - FILE_EXT_CLASS.length());
+						className = className.replace('/', '.');
+						Class<?> clz = cl.loadClass(className);
+						if (classFilter == null || (classFilter != null && classFilter.isAssignableFrom(clz))) {
+							clazzesFound.add(clz);
+						}
+					}
+				}
+			} catch (MalformedURLException e1) {
+				e1.printStackTrace();
+				continue;
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
+				continue;
+			}
+		}
+		return new ArrayList<>(clazzesFound);
+	}
+
+	public List<Class> findAllClasses(String packageName, List<JarFile> jarFiles) {
+		return findAllClasses(packageName, null, jarFiles);
+	}
+
+	public List<JarFile> findAllUdfJars(List<String> sourceLocations) {
+		Set<JarFile> jarFiles = new HashSet<>();
+		for (String eachLocation : sourceLocations) {
+			Path p = Paths.get(eachLocation);
+			File f = p.toFile();
+			if (f.isFile()) {
+				f = p.getParent().toFile();
+			}
+			try {
+				File[] files = f.listFiles(new FilenameFilter() {
+					public boolean accept(File dir, String name) {
+						return name.toLowerCase().endsWith(".jar");
+					}
+				});
+				if (files != null) {
+					for (File eachJarFile : files) {
+						JarFile jarFile = new JarFile(eachJarFile.getAbsolutePath());
+						jarFiles.add(jarFile);
+					}
+				}
+			} catch (IOException | SecurityException e) {
+				e.printStackTrace();
+			}
+		}
+		return new ArrayList<>(jarFiles);
 	}
 
 	public List<Resource> loadIncludeLibraries(ResourceSet resourceSet, String[] includeFilenames) throws IOException {
@@ -38,6 +145,48 @@ public class BDSLLib {
 			resources.add(resource);
 		}
 		return resources;
+	}
+
+	public void addToClasspath(String... files) {
+		for (String f : files) {
+			addToClasspath(f);
+		}
+	}
+
+	public void addToClasspath(JarFile... files) {
+		for (JarFile f : files) {
+			addToClasspath(f.getName());
+		}
+	}
+
+	public void addToClasspath(File... files) {
+		for (File f : files) {
+			addToClasspath(f);
+		}
+	}
+
+	public void addToClasspath(String file) {
+		addToClasspath(new File(file));
+	}
+
+	public void addToClasspath(File file) {
+		try {
+			if (!file.exists())
+				throw new FileNotFoundException(file.getPath());
+
+			URL url = file.toURI().toURL();
+			URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+			Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+			method.setAccessible(true);
+			method.invoke(classLoader, url);
+			loadedJarFiles.add(file);
+		} catch (Exception e) {
+			throw new RuntimeException("Exception loading external jar file.", e);
+		}
+	}
+	
+	public List<File> getLoadedJarFiles() {
+		return loadedJarFiles;
 	}
 
 }
